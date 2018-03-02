dd64a03f987927eae0cc28dc2562c393233c7dfe
Jérémy Laurens <jeremy.laurens@Digital-Grenoble.local>
2018-03-02 15:15:03 +0100

    toggle Ok

--- monProjet/src/App.vue
+++ monProjet/src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" width="200px">
+    <img src="./assets/logo.png" width="275px">
+
+
+  <ul id="example-1">
+  <li v-for="machine in machines">
+    {{ machine.name }} <toggle-button :value=" machine.status  " 
+               :labels="{checked: 'Activé', unchecked: 'Désactivé'}"/>
+    </li>
+  </ul>
+
+
+
+</br></br>
+
+    <h2>--------------------------------------------------------------------------</h2></br>
     <h3> Machines à café n°1 </h3>    {{ machine1 }}
     <h3 v-if="machine1"><img src="https://startupsavant.com/wp-content/uploads/2014/01/Validate-your-business-idea.png" alt="Smiley face" height="50px"></h3>
     <h3 v-else><img src="https://alyssabethancourt.files.wordpress.com/2014/07/no.gif" alt="Smiley face" height="50px"></h3>
     <button @click="machine1 = !machine1" :aria-expanded="machine1 ? 'true' : 'false'">
     Activer/Désactiver 
     </button> </br></br></br>
+
     <h2>--------------------------------------------------------------------------</h2></br>
 
 
@@ -21,16 +36,34 @@
 </template>
 
 <script>
+
 export default {
   name: 'app',
   data () {
     return {
       machine1: true,
-      machine2: false
-    }
-    
+      machine2: false,
+      machines: [
+      {
+        name: "Machine1",
+        status: true
+      },
+      {
+        name: "Machine2",
+        status: false
+      },
+      {
+        name: "Machine3",
+        status: true
+      }
+      
+      ]
+      
+    } 
   }
 }
+
+
 </script>
 
 <style lang="scss">
@@ -53,7 +86,6 @@ ul {
 }
 
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 
--- monProjet/src/main.js
+++ monProjet/src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ToggleButton from 'vue-js-toggle-button'
+Vue.use(ToggleButton)
 
+Vue.component('my-component', {
+  template: '<div><li v-for="machine in machines">{{ machine.name }}</li></div>'
+})
+
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
 new Vue({
   el: '#app',
   render: h => h(App),
   data: {
-    machine1: "active",
-    machine2: "inactive"
-  },
+    machines: [
+      { name: 'Machine 1' },
+      { name: 'Machine 2' }
+    ]
+  }
 })
 
